e82f04f0f3e6d8fd27581b23b5d746b10ff62fd7
Jacquiiii <jacqui.koroll@telus.com>
2023-01-10 19:11:40 -0800

    remove hardcoded interviewer obj from InterviewerList component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,19 +3,10 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
 
-const interviewers = [
-  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
-  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
-  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
-  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
-  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
-];
-
-
 // renders a list of InterviewerListItem components
 export default function InterviewerList(props) {
 
-  const interviewerListItems = interviewers.map(interviewer => {
+  const interviewerListItems = props.interviewers.map(interviewer => {
 
     return (
       <InterviewerListItem 
