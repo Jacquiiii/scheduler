dfa788075a0788ece48a4df435250d4c627cc7e7
Jacquiiii <jacqui.koroll@telus.com>
2023-01-10 19:59:59 -0800

    refactor prop names on DayList and InterviewerList components

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -29,7 +29,7 @@ export default function DayListItem(props) {
   const spots = formatSpots(props.spots);
 
   return (
-    <li onClick={() => props.setDay(props.name)} className={dayClass}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{spots}</h3>
     </li>
