f2c4296829de0b5ad74f354b176c9d250d61f422
Jacquiiii <jacqui.koroll@telus.com>
2023-01-10 19:03:36 -0800

    add InterviewerList which renders list of InterviewerListItem components

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+
+// renders a list of InterviewerListItem components
+export default function InterviewerList(props) {
+
+  const interviewerListItems = interviewers.map(interviewer => {
+
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        id={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      />
+    );
+
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerListItems}</ul>
+    </section>
+  );
+
+};
