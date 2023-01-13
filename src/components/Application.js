a7ac21fc4ee25581f89d44415568fa96cf2a848d
Jacquiiii <jacqui.koroll@telus.com>
2023-01-12 16:53:40 -0800

    add promise.all to fetch data from api to update state

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,46 +4,7 @@ import axios from 'axios';
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-
-
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import getAppointmentsForDay from "helpers/selectors";
 
 
 // single page app content
@@ -52,29 +13,38 @@ const Application = (props) => {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    // appointments: {}
+    appointments: {}
   });
 
   // updates state with a new day
   const setDay = day => setState({ ...state, day });
 
-  // updates state with a new days array
-  const setDays = days => setState(prev => ({ ...prev, days }));
-
-  // gets days data from API and updates state with days data
+  // gets days and appointments data from API and updates state with new days array and appointments object
   useEffect(() => {
-    axios.get("/api/days").then((response) => setDays(response.data));
+
+    Promise.all([
+      axios.get('/api/days'),
+      axios.get('/api/appointments')
+    ]).then((all) => {
+      setState(prev => ({
+        ...prev,
+        days: all[0].data,
+        appointments: all[1].data
+      }));
+    });
+
   }, [])
 
   // produces list of Appointment components to be displayed on the page
-  const appointmentList = Object.values(appointments).map(appointment => {
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const appointmentList = dailyAppointments.map(appointment => {
     return (
       <Appointment
         key={ appointment.id }
         { ...appointment }
       />
     );
-  }) 
+  })
 
   return (
     <main className="layout">
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay (state, day) {
+export default function getAppointmentsForDay (state, day) {
 
   // returns object matching day
   const filteredDay = state.days.find(d => d.name === day);
